3973872b590fd8cb48455baf4949899f4cd992be
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2024-09-29 06:20:22 +0000

    区間の Membership 追加

--- Project/RealInterval/RealNumberIntervalOperations.lean
+++ Project/RealInterval/RealNumberIntervalOperations.lean
@@ -2,6 +2,10 @@ import Project.RealInterval.Basic
 
 namespace RealInterval
 
+-- Membership of a real number in an interval
+instance : Membership ℝ Interval :=
+  ⟨λ x α ↦ x.inf ≤ α ∧ α ≤ x.sup⟩
+
 -- Scalar multiplication of intervals using Mathlib's scalar multiplication
 noncomputable instance : SMul ℝ Interval :=
   ⟨λ c x ↦ {
